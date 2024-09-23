cd5bb9ee7fb1d2151a5b80723fcd53b737f5175c
ychong4 <ychong4@unl.edu>
2024-09-23 14:36:20 -0500

    1

--- etl.py
+++ etl.py
@@ -6,34 +6,30 @@ import s3fs
 
 import googleapiclient.discovery
 
+def run_youtube_etl():
+	# Disable OAuthlib's HTTPS verification when running locally.
+	# *DO NOT* leave this option enabled in production.
+	os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
-# Disable OAuthlib's HTTPS verification when running locally.
-# *DO NOT* leave this option enabled in production.
-os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+	api_service_name = "youtube"
+	api_version = "v3"
+	DEVELOPER_KEY = "AIzaSyChRV4Vsu1Srvr1JUCYSBqkglXzo13GGds"
 
-api_service_name = "youtube"
-api_version = "v3"
-DEVELOPER_KEY = "AIzaSyChRV4Vsu1Srvr1JUCYSBqkglXzo13GGds"
+	youtube = googleapiclient.discovery.build(
+	api_service_name, api_version, developerKey = DEVELOPER_KEY)
 
-youtube = googleapiclient.discovery.build(
-api_service_name, api_version, developerKey = DEVELOPER_KEY)
+	#request = youtube.commentThreads().list(
+	#    part="snippet, replies",
+	#    videoId="q8q3OFFfY6c"
+	#)
+	request = youtube.commentThreads().list(
+		part="snippet, replies",
+		#parentId="UgzDE2tasfmrYLyNkGt4AaABAg"
+		videoId = "ndAQfTzlVjc"
+	)
 
-#request = youtube.commentThreads().list(
-#    part="snippet, replies",
-#    videoId="q8q3OFFfY6c"
-#)
-request = youtube.commentThreads().list(
-    part="snippet, replies",
-    #parentId="UgzDE2tasfmrYLyNkGt4AaABAg"
-    videoId = "ndAQfTzlVjc"
-)
-
-response = request.execute()
-
-#print(response)
-
-comments = []
-def process_comments(response):
+	response = request.execute()
+	comments = []
 	items = response.get('items', [])
 	
 	for item in items:
@@ -44,14 +40,8 @@ def process_comments(response):
 						'comment': comment_text, 'published_at': publish_time}
 
 			comments.append(comment_info)
-	print(f'Finished processing {len(comments)} comments.')
-	return comments
-
-process_comments(response)
-print(comments)
-
-df = pd.DataFrame(comments)
-df.to_csv("comments.csv")
+	df = pd.DataFrame(comments)
+	df.to_csv("s3://myairflowyoutubebucket/comments.csv")
 
 
 
