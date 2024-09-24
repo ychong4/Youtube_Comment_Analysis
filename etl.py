2ae95617840b24dd7812ac32de847cf0560ab4ac
ychong4 <ychong4@unl.edu>
2024-09-24 08:03:34 -0500

    1

--- etl.py
+++ etl.py
@@ -7,55 +7,72 @@ import s3fs
 import googleapiclient.discovery
 
 def run_youtube_etl():
-	# Disable OAuthlib's HTTPS verification when running locally.
-	# *DO NOT* leave this option enabled in production.
-	os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+    # Disable OAuthlib's HTTPS verification when running locally.
+    # *DO NOT* leave this option enabled in production.
+    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
-	api_service_name = "youtube"
-	api_version = "v3"
-	DEVELOPER_KEY = "AIzaSyChRV4Vsu1Srvr1JUCYSBqkglXzo13GGds"
-	next_page_token = None
+    api_service_name = "youtube"
+    api_version = "v3"
+    DEVELOPER_KEY = "AIzaSyChRV4Vsu1Srvr1JUCYSBqkglXzo13GGds"
+    next_page_token = None
 
-	youtube = googleapiclient.discovery.build(
-	api_service_name, api_version, developerKey = DEVELOPER_KEY)
+    # Build YouTube API service
+    youtube = googleapiclient.discovery.build(
+        api_service_name, api_version, developerKey=DEVELOPER_KEY
+    )
+    
+    comments = []
+    total_comments = 0
+    limit = 5000  # Set the limit to 5000 comments
 
-	while True:
-	
-		request = youtube.commentThreads().list(
-			part="snippet, replies",
-			videoId = 'D7GDTOSNQSk',
-			#"ndAQfTzlVjc",
-			pageToken=next_page_token,
-            maxResults=100	
-		)
+    # Loop to fetch all comments, handling pagination
+    while True:
+        request = youtube.commentThreads().list(
+            part="snippet,replies",
+            videoId='D7GDTOSNQSk',  # YouTube video ID
+            pageToken=next_page_token,
+            maxResults=100  # Fetch 100 comments per page
+        )
 
-		response = request.execute()
-		comments = []
-		items = response.get('items', [])
-		
-		for item in items:
-			author = item['snippet']['topLevelComment']['snippet']['authorDisplayName']
-			comment_text = item['snippet']['topLevelComment']['snippet']['textOriginal']
-			publish_time = item['snippet']['topLevelComment']['snippet']['publishedAt']
-			comment_info = {'author': author, 
-							'comment': comment_text, 'published_at': publish_time}
+        response = request.execute()
+        items = response.get('items', [])
 
-			comments.append(comment_info)
-		#print(comments)
+        # Extract comments from each item
+        for item in items:
+            author = item['snippet']['topLevelComment']['snippet']['authorDisplayName']
+            comment_text = item['snippet']['topLevelComment']['snippet']['textOriginal']
+            publish_time = item['snippet']['topLevelComment']['snippet']['publishedAt']
+            comment_info = {
+                'author': author,
+                'comment': comment_text,
+                'published_at': publish_time
+            }
 
-		next_page_token = response.get('nextPageToken')
+            comments.append(comment_info)
+            total_comments += 1
 
-        #if next_page_token is None:
-        #break
-		#print(next_page_token)
-		#print(comments)
-		df = pd.DataFrame(comments)
-		df.to_csv("comments.csv")
-	#s3://myairflowyoutubebucket/
-	print(comments)
+            # Stop if we have reached the limit of 5000 comments
+            if total_comments >= limit:
+                break
 
-	return comments
-	
+        # Break if we hit the limit in the middle of a page
+        if total_comments >= limit:
+            break
+
+        # Get the next page token for pagination
+        next_page_token = response.get('nextPageToken')
+
+        # Break the loop if no more pages are left
+        if next_page_token is None:
+            break
+
+    # Convert the comments list to a DataFrame and save to CSV
+    df = pd.DataFrame(comments)
+    df.to_csv("comments.csv", index=False)
+
+    return comments
+
+
 	
 
 
